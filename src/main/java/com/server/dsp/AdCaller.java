55cf5df7a42759d98cd439a28a5e60dd36694666
dongbeom-kim <dongbeom-kim@nhnent.com>
2018-12-13 16:01:48 +0900

    first commit server-dsp

---
+++ src/main/java/com/server/dsp/AdCaller.java
@@ -0,0 +1,11 @@
+package com.server.dsp;
+
+public class AdCaller {
+
+    public double calculateEctrValue() {
+        System.out.println("make String Array flied");
+        //Call Ectr-Learner predict(String[] arr)
+        double resultEctrValue = 0; //return predict method
+        return resultEctrValue;
+    }
+}
