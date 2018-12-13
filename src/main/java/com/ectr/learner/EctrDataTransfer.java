0551eb674d459c8c9190c7c53576ec6e3b81569e
dongbeom-kim <dongbeom-kim@nhnent.com>
2018-12-13 15:45:34 +0900

    add method and class

---
+++ src/main/java/com/ectr/learner/EctrDataTransfer.java
@@ -0,0 +1,10 @@
+package com.ectr.learner;
+
+public class EctrDataTransfer {
+
+    //called by ectr-commander
+    public Object transferBackupData() {
+        System.out.println("double array convert to object");
+        return new Object();
+    }
+}
