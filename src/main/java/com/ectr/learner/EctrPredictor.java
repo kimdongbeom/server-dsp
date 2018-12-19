c65134af99538f4a56ed472aa1da09e583259212
dongbeom-kim <dongbeom-kim@nhnent.com>
2018-12-20 08:23:09 +0900

    predict값 변경

--- src/main/java/com/ectr/learner/EctrPredictor.java
+++ src/main/java/com/ectr/learner/EctrPredictor.java
@@ -4,7 +4,8 @@ public class EctrPredictor {
 
     public double predictEctrValue(String[] inputArr) {
         System.out.println("predict using inputArr ");
-        double ectrResult = 0.003;
+        double ectrResult = 0.059;
+        System.out.println("change value");
         return ectrResult;
     }
 }
